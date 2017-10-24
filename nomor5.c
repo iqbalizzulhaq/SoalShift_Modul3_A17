c35cad97de805de20e730d0e8b235843d620075d
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-24 21:33:17 +0700

    ntaps

--- nomor5.c
+++ nomor5.c
@@ -7,3 +7,45 @@
 
 pthread_t tid[2];
 
+void* count1(void *arg)
+{
+
+pthread_t=pthread_self():
+if (pthread_equal(id,tid[0]))
+{
+FILE *in;
+char *line=NULL;
+size_t len=0;
+ssize_t read;
+int i;;
+int jumlah=0;
+in=fopen("/home/asus/novel.txt","r");
+while(getline(&line, &len,in)!=-1){
+
+	if (strstr(line,"fina")!=NULL) {
+jumlah++;
+ 					}
+				}
+	}
+else if (pthread_equal(id,tid[1]))
+{
+FILE *in;
+char *line=NULL;
+size_t len=0;
+ssize_t read;
+int i;char fina;
+int jumlah=0;
+in=fopen("/home/asus/novel.txt","r");
+while(getline(&line, &len,in)!=-1){
+
+if (strstr(line,"ifah")!=NULL) {
+jumlah++;
+   				}
+			}
+
+	}
+
+
+}
+
+
