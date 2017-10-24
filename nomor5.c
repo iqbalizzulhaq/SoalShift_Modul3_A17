09d34f7ca380ca79b8f0dbb2154764608716e41d
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-24 23:05:15 +0700

    rubah

--- nomor5.c
+++ nomor5.c
@@ -6,46 +6,45 @@
 #include<stdlib.h>
 
 pthread_t tid[2];
+FILE *in;
+char *line=NULL;
+size_t len=0;
+
+int i;
+int jumlah=0;
 
 void* count1(void *arg)
 {
 
-pthread_t=pthread_self():
+pthread_t id=pthread_self();
 if (pthread_equal(id,tid[0]))
 {
-FILE *in;
-char *line=NULL;
-size_t len=0;
-ssize_t read;
-int i;;
-int jumlah=0;
 in=fopen("/home/asus/novel.txt","r");
 while(getline(&line, &len,in)!=-1){
 
 	if (strstr(line,"fina")!=NULL) {
-jumlah++;
- 					}
+			jumlah++;}
 				}
 	}
+
 else if (pthread_equal(id,tid[1]))
 {
-FILE *in;
-char *line=NULL;
-size_t len=0;
-ssize_t read;
-int i;char fina;
-int jumlah=0;
 in=fopen("/home/asus/novel.txt","r");
 while(getline(&line, &len,in)!=-1){
 
-if (strstr(line,"ifah")!=NULL) {
-jumlah++;
-   				}
+	if (strstr(line,"ifah")!=NULL) {
+		jumlah++;}
 			}
 
 	}
 
+return NULL;
+}
+int main (void)
+{
+
+
+
 
 }
-
 
