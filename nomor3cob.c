c74192cae62cef038cd12d347f8c8876376530dc
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-25 14:48:18 +0700

    hi

--- nomor3cob.c
+++ nomor3cob.c
@@ -13,6 +13,11 @@ printf ("1.lohan =%d\n",stat1);
 printf ("2.kepiting =%d\n",stat2);
 printf ("beri makan = ");
 scanf("%d",&act);
+printf("-------------\n");
+if (stat1<=50){printf("lohan lapar\n");}
+else {printf("lohan kenyang\n");}
+if (stat2<=50){printf ("kepiting lapar\n");}
+else {printf("kepiting kanyang\n");}
 }
 void *kurang1(){
 sleep(10);
@@ -37,7 +42,7 @@ pthread_create(&(thkurang1),NULL,&kurang1,NULL);
 pthread_create(&(thkurang2),NULL,&kurang2,NULL);
 pthread_create(&(thtambah),NULL,&tambah,NULL);
 if((stat1<=0||stat1>100)||(stat2<=0||stat2>100)){
-printf("\n selesai"); 
+printf("\n selesai\n"); 
 break;
 	}
 pthread_join(thmenu,NULL);
