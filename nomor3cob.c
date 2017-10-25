602c24d5389443f2cc519c167503f89cdb08b7de
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-25 12:48:53 +0700

    pushh

---
+++ nomor3cob.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t thmenu,thtambah,thkurang1,thkurang2;
+
+void *menu(){
+int stat1=100,stat2=100,act;
+printf ("1.lohan =%d",stat1);
+printf ("2.kepiting =%d",stat2);
+printf ("beri makan = %d",act);
+}
+void *kurang1(){
+sleep(3);
+stat1 -=15;
+}
+void *kurang2(){
+sleep(3);
+stat2 +=20;
+}
+void *tambah(){
+if (act==1){
+ 	stat1 +=10;}
+if (act==2){
+	stat2 +=10;}
+}
+
+int main(){
+while(1){
+
+
