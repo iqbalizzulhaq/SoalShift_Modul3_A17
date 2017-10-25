f598ffe46c4b6271be88b9b0f736f646de3b8c69
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-25 13:46:12 +0700

    ssiip

--- nomor3cob.c
+++ nomor3cob.c
@@ -6,12 +6,13 @@
 #include<stdlib.h>
 
 pthread_t thmenu,thtambah,thkurang1,thkurang2;
+int stat1=100,stat2=100,act;
 
 void *menu(){
-int stat1=100,stat2=100,act;
-printf ("1.lohan =%d",stat1);
-printf ("2.kepiting =%d",stat2);
-printf ("beri makan = %d",act);
+printf ("1.lohan =%d\n",stat1);
+printf ("2.kepiting =%d\n",stat2);
+printf ("beri makan = ");
+scanf("%d",&act);
 }
 void *kurang1(){
 sleep(3);
@@ -19,23 +20,26 @@ stat1 -=15;
 }
 void *kurang2(){
 sleep(3);
-stat2 +=20;
+stat2 -=20;
 }
 void *tambah(){
 if (act==1){
  	stat1 +=10;}
-if (act==2){
-	stat2 +=10;}
+else if (act==2){
+ 	stat2 +=10;}
 }
 
 int main(){
 while(1){
-pthread_create(&(thmenu)),NULL,&menu,NULL);
-pthread_create(&(thkurang1)),NULL,&kurang1,NULL);
-pthread_create(&(thkurang2)),NULL,&kurang2,NULL);
-pthread_create(&(thtambah)),NULL,&tambah,NULL);
 
-
-
+pthread_create(&(thmenu),NULL,&menu,NULL);
+pthread_create(&(thkurang1),NULL,&kurang1,NULL);
+pthread_create(&(thkurang2),NULL,&kurang2,NULL);
+pthread_create(&(thtambah),NULL,&tambah,NULL);
+if((stat1<=0||stat1>100)||(stat2<=0||stat2>100)){
+printf("\n selesai"); 
+break;
+	}
+pthread_join(thmenu,NULL);
 }
-
+}
