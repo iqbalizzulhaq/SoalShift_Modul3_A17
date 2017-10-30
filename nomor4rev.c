3e20b6728ba1e5f29880a59f19b3c4ba0b964cbe
Unknown <iqbal.izzulhaq3@gmail.com>
2017-10-30 09:15:32 +0700

    fungsi faktorial

---
+++ nomor4rev.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+  int c, n, fact = 1;
+ 
+  printf("Enter a number to calculate it's factorial\n");
+  scanf("%d", &n);
+ 
+  for (c = 1; c <= n; c++)
+    fact = fact * c;
+ 
+  printf("Factorial of %d = %d\n", n, fact);
+ 
+  return 0;
+}
--- nomor5rev.c
+++ nomor5rev.c
@@ -7,28 +7,31 @@
 char kata[50];
 char string[60];
 int num=0;
+
 pthread_t tid[50];
 
 void *count(void *arg){
 	char **kata = (char**)arg;
-FILE *in,
-in= fopen("/home/asus/git/novel.txt","r");
+
+FILE *in;
+in = fopen("/home/asus/git/Novel.txt","r");
 scanf("%s",kata);
 while (fscanf(in,"%s",string)==1)
 {
-   if (strstr(string,kata)!=0{
+   if (strstr(string,kata)!=0){
 		num++;
-}
+				}
 
 printf("%s %d\n",kata,num);
 fclose(in);
 }
 
-int main (int argc, char *argv[]{
- for (kata=1;kata<argc;kata++){
+
+int main (int argc, char *argv[]){
+for (kata=1;kata<argc;kata++){
 
  pthread_create(&tid[kata],NULL,count,(void*)argv;
  pthread_join(tid[kata],NULL);
-}
+				}
 return 0;
 }
